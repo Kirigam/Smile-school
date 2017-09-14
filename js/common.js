bb7edeac185fd63d9cffa39ba7d98ac35cf6bf1a
Kirigam <hrytsiv.a.s@gmail.com>
2017-09-15 00:56:24 +0300

    Made UI

---
+++ js/common.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+
+ $('.sl').slick({	
+ 	autoplay: true,
+ 	autoplaySpeed: 3000,
+ 	cssEase: 'ease-in',
+ 	speed: 600,
+ 	dots: true,
+ 	arrows: false,
+ 	fade: true,
+  });
+ });
