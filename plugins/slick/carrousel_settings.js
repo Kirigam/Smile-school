7c2a9d60f498943ac104bc641b483aa99ca158c1
ffastym <yuriy.matviyuk22@gmail.com>
2017-09-16 21:09:18 +0300

    header + slider

---
+++ plugins/slick/carrousel_settings.js
@@ -0,0 +1,19 @@
+﻿/* Start slider1 */
+
+$(document).on('ready', function() {
+	var value = true;
+	if($(window).width() > 640) { value = true} else {value = false};
+  $(".single-slide").slick({
+	dots: true,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: value,
+	autoplaySpeed: 5000,
+	arrows: false,
+  });
+  
+
+});
+
+/* /Carrousel of dishes; */
