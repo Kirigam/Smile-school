bce03113096a0a5b5fd126a9ea7d7ea3083edccb
Andrew.Dol <adolsnskiy@gmail.com>
2017-09-17 15:44:43 +0300

    new slider

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ $(document).ready(function() {
   });
   
 });
+
+$(document).ready(function() {
+    var sync2 = $("#sync2");
+    var slidesPerPage = 5; //globaly define number of elements per page
+    sync2.owlCarousel({
+        items : 1,
+        slideSpeed : 5000,
+        nav: false,
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsiveRefreshRate : 5000
+    }).on('changed.owl.carousel');
+    $(".owl-next").text("›");
+    $(".owl-prev").text("‹");
+});
