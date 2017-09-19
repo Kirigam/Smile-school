f92fb4fb059a3f17ec0fb522e7d2b63745251b76
Andrew.Dol <adolsnskiy@gmail.com>
2017-09-19 23:16:38 +0300

    grid system

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
- 
-  $("#owl-example").owlCarousel({
-      itemsDesktop : [1499,1],
-      itemsDesktopSmall : [1199,1],
-      itemsTablet : [899,1],
-      itemsMobile : [599,1],
-      navigation : true,
-      autoplay: true,
-      navigationText : ['<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span>','<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span>'],
-  });
-  
-});
-
-$(document).ready(function() {
     var sync2 = $("#sync2");
     var slidesPerPage = 5; //globaly define number of elements per page
     sync2.owlCarousel({
@@ -27,3 +13,12 @@ $(document).ready(function() {
     $(".owl-next").text("›");
     $(".owl-prev").text("‹");
 });
+function bar(){
+	var vision = $("#manyBars"); 
+	if (vision.css("display") == "none") {
+		vision.css("display", "block");
+	}
+	else{
+		vision.css("display", "none");
+	}
+}
