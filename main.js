733692cd494ffd1b8356abc5f004c784b27ccf3e
Andrew.Dol <adolsnskiy@gmail.com>
2017-09-25 16:54:28 +0300

    added collape

--- main.js
+++ main.js
@@ -24,25 +24,62 @@ $(document).ready(function() {
     var prev = $(".owl-prev");
     next.text("»");
     prev.text("«");
-    var footerHeight = parseInt($("#footerImg").css("height"));
-    margin = (footerHeight/2 - 50) * (-1);
-    next.css("margin-top", (margin+"px").toString());
-    prev.css("margin-top", (margin+"px").toString());
+    navbar(50);
 });
-function navbar(){
+function navbar(n = 25){
     var next = $(".owl-next");
     var prev = $(".owl-prev");
     var footerHeight = parseInt($("#footerImg").css("height"));
-    margin = (footerHeight/2 - 25) * (-1);
+    margin = (footerHeight / 2 - n) * (-1);
     next.css("margin-top", (margin+"px").toString());
     prev.css("margin-top", (margin+"px").toString());
 }
 function bar(){
-	var vision = $("#manyBars"); 
-	if (vision.css("display") == "none") {
-		vision.css("display", "block");
-	}
-	else{
-		vision.css("display", "none");
-	}
+    
+}
+function collapse(blocks, height, button = false){
+    var items = $(blocks);
+    var height = parseInt($(height).css("height"));
+    var nowHeight = parseInt(items.css("height"));
+    var bool = false;
+    if (button != false) {
+        var but = $(button);
+        if (nowHeight <= 5) {
+            bool = true;
+            but.css("transform", "rotate(270deg)");
+            but.css("margin", "20px 0 -20px -21px");
+        }
+        else{
+            but.css("transform", "rotate(90deg)");
+            but.css("margin", "0 0 -20px 21px");
+        }
+    }
+    else{
+        if (nowHeight <= 5) {
+            bool = true;
+        }
+    }
+    var i = nowHeight;
+    var myFor = setInterval(function(){
+        items.css("height", i.toString() + "px");
+        if (bool) {
+            i += 5;
+        }
+        else{
+            i -= 5;
+        }
+        if (i > height - 5 ) {
+            clearInterval(myFor);
+            if (button != false) {
+                items.css("height", "auto");
+            }
+            else{
+                items.css("height", height);
+            }
+        }
+        if (i < 5) {
+            clearInterval(myFor);
+            items.css("height", "0");
+        }
+    }, 1);
 }
