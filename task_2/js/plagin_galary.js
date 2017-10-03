79097058795cac5a20fca07fc2bc7454db9f6436
Andrew.Dol <adolsnskiy@gmail.com>
2017-10-03 21:40:45 +0300

    task_3

--- task_2/js/plagin_galary.js
+++ task_2/js/plagin_galary.js
@@ -17,28 +17,25 @@
         * add items to the DOM
         */
         return $(this).on('click', function () {
-          imgHtml ="<div class=\"bigImg\">\n" + "<img class=\"imgNow\" src=" + self.src + ">\n" + "</div>\n";
-          html = $.parseHTML("<div class=\"galaryBackground\">\n" + "<div class=\"bigImgBlock\">\n" + imgHtml + "</div>\n" + "</div>\n");
-          $("body").append(html);
-          if (defaulSettings.button == true) {
-            $(".galaryBackground").append($.parseHTML("<div class=\"galaryBut galaryPrev\"><div class=\"fa fa-caret-down fa-5x triangle\"></div></div>"));
-            $(".galaryBackground").append($.parseHTML("<div class=\"galaryBut galaryNext\"><div class=\"fa fa-caret-down fa-5x triangle\"></div></div>"));
-          }
-          
+          var bigImg = $('.galaryBackground').css("display", "block");
+          $(".bigImg img").attr("src", this.src);
 
+          if (defaulSettings.button == false) {
+              $(".galaryBut").css("display", "none");
+          }
           /**
           * close galary
           */
           $(window).on('keydown', function(e) {
             if (e.keyCode == 27) {
-              $(".galaryBackground").remove();
+              $(".galaryBackground").css("display", "none");
             }
           });
           $(document).mouseup(function (e) {
             var img = $(".bigImgBlock");
             var button = $(".galaryBut");
             if (img.has(e.target).length === 0 && button.has(e.target).length === 0){
-              $(".galaryBackground").remove();
+              $(".galaryBackground").css("display", "none");
             }
           });
 
@@ -48,35 +45,36 @@
           * button next img and prev img
           */
           if (defaulSettings.button == true) {
-            $('.galaryNext').on('click', function(){
-              var now = $(".bigImg img").attr("src"),
-              next;
+            $('.galaryBut').on('click', function(){
+              console.log("fuck");
+              var button = $(this).attr('class').split(' '),
+              now = $(".bigImg img").attr("src"),
+              next,
+              prev;
               for(var i = 0; i < allImg.length; i++){
                 if (i == allImg.length - 1) {
                   next = allImg[0].src;
+                  prev = allImg[i - 1].src;
+                  break;
+                }
+                if (allImg[i].src == now && i == 0) {
+                  prev = allImg[allImg.length - 1].src;
+                  next = allImg[i + 1].src;
                   break;
                 }
                 if (allImg[i].src == now) {
                   next = allImg[i + 1].src;
-                  break;
-                }
-              }
-              $(".bigImg img").attr("src", next);
-            });
-            $('.galaryPrev').on('click', function(){
-              var now = $(".bigImg img").attr("src"),
-              prev;
-              for(var i = 0; i < allImg.length; i++){
-                if (allImg[i].src == now && i == 0) {
-                  prev = allImg[allImg.length - 1].src;
-                  break;
-                }
-                if (allImg[i].src == now) {
                   prev = allImg[i - 1].src;
                   break;
                 }
               }
-              $(".bigImg img").attr("src", prev);
+              if (button[1] == "galaryNext") {
+                $(".bigImg img").attr("src", next);
+                console.log(next);
+              }
+              else{
+                $(".bigImg img").attr("src", prev);
+              }
             });
           }
         });
