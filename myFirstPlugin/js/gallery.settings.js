beb04fbd90df3e7615fe6a1ff1e6c49f924d8d9c
ffastym <yuriy.matviyuk22@gmail.com>
2017-10-04 18:38:31 +0300

    add widget

--- myFirstPlugin/js/gallery.settings.js
+++ myFirstPlugin/js/gallery.settings.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     $('.gallery').galleryPlugin({
-        margin: '150px auto',
-        width : '966px',
         description: true
     });
 });
--- myFirstPlugin/js/smile.plugin.gallery.js
+++ myFirstPlugin/js/smile.plugin.gallery.js
@@ -1,9 +1,6 @@
 (function ($) {
     $.fn.galleryPlugin = function (options) {
         var defaultSettings = $.extend({
-            margin: '150px auto',
-            width: '650px',
-            display: 'none',
             description: false
     }, options);
     return this.each(function () {
@@ -17,37 +14,38 @@
         }).click(function() {
             var index = $(this).index('img');
             var link = imagesArr[index];
-            var buttons = '<button class="prev"></button><button class="next"></button><button class="exit"></button>';
-            var zoomLayer = '<div class="zoom"><div><img src="' + link + '">' + buttons +'</div></div>';
+            var buttons = {
+                prevBtn: '<button class="prev arrows"></button>',
+                nextBtn: '<button class="next arrows"></button>',
+                exitBtn: '<button class="exit"></button>'
+            };
+            var buttonsArr = buttons.prevBtn + buttons.nextBtn + buttons.exitBtn;
+            var zoomLayer = '<div class="zoom"><div><img src="' + link + '">' + buttonsArr +'</div></div>';
             $('body').append(zoomLayer);    //add container of zoom images
             if (defaultSettings.description) {
-                $('.zoom').find('div').append('<span class="description"></span>');
+                $('.zoom').find('div').append('<span class="description">' + descriptionsArr[index] + '</span>');
             }
-            $('.zoom').find('.description').text(descriptionsArr[index]);
             $('.exit').click (function(){
                 $(".zoom").remove()     //remove container of zoom images
             });
-            $('.prev').click(function() {   //change image to previous
-                var linkBack = imagesArr[--index];
-                if (index == -1) {
-                    index = imagesCount - 1;
-                    linkBack = imagesArr[imagesCount - 1];
+            $('.arrows').click(function() {
+                if ($(this).hasClass('prev')) {
+                    var toLink = imagesArr[--index];
+                    if (index == -1) {
+                        index = imagesCount - 1;
+                        toLink = imagesArr[imagesCount - 1];
+                    }
+                 } else {
+                    toLink = imagesArr[++index];
+                    if (index == imagesCount) {
+                        index = 0;
+                        toLink = imagesArr[0];
+                    }
                 }
-                $('.zoom').find('img').attr('src', linkBack);
-                $('.zoom').find('.description').text(descriptionsArr[index]);   //add description to zooming image
-            });
-            $('.next').click(function() {   //change image to next
-                var linkNext = imagesArr[++index];
-                if (index == imagesCount) {
-                    index = 0;
-                    linkNext = imagesArr[0];
-                }
-                $('.zoom').find('img').attr('src', linkNext);
+                $('.zoom').find('img').attr('src', toLink);
                 $('.zoom').find('.description').text(descriptionsArr[index]);   //add description to zooming image
             })
         });
-        $(self).css('margin', defaultSettings.margin);
-        $(self).css('width', defaultSettings.width);
     });
     };
 })(jQuery);
