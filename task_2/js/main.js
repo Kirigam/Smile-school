19271040b7964b41cdb1b75842c063a000e9603d
Andrew.Dol <adolsnskiy@gmail.com>
2017-10-06 23:54:51 +0300

    galary in prodaction

--- task_2/js/main.js
+++ task_2/js/main.js
@@ -1,5 +1,11 @@
  $(document).ready(function () {
    $('.galaryCon').smile({
    		button: true,
+   		background: "ff68d8",
+   		alpha: 0.6,
+   		autoplay: {
+   			bool: true,
+   			interval: 1000
+   		},
     });
 });
--- task_2/js/plagin_galary.js
+++ task_2/js/plagin_galary.js
@@ -1,33 +1,51 @@
  (function ($) {
+    function hexToRGB(hex, alpha) {
+      var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+      return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    }
    /**
     * @param options
     * @returns {*}
-    * динамічно додавти класи
-    * масив обь'єктів з тим що треба
-    * дада-індекс attr(data-index, i++)
-    * кнопки рендерити динамічно
     */
     $.fn.smile = function (options) {
       var defaulSettings = $.extend({
          button: true,
+         background: "000000",
+         alpha: 0.8,
+         autoplay: {
+          bool: false,
+          interval: 3000 
+         }
       }, options);
       var html,
-      imgHtml,
-      allImg = [];
-      $(this).find('div').addClass("blockImg col-4-12").find('img').addClass("galaryImg");
-      this.each(function () {
+      allImg = [],
+      imgArray = $(this).find('img'),
+      i = 0,
+      backColor = hexToRGB(defaulSettings.background, defaulSettings.alpha);
+      $(this).find('div').addClass("blockImg").find('img').addClass("galaryImg");
+      html = $.parseHTML("<div class=\"galaryBackground\">\n <div class=\"bigImgBlock\">\n <div class=\"bigImg\">\n <img class=\"imgNow\" src=\"\">\n </div>\n </div>\n </div>\n");
+      $("body").append(html);
+      if (defaulSettings.button == true) {
+        $(".galaryBackground").append($.parseHTML("<div class=\"galaryBut galaryPrev\"><div class=\"fa fa-caret-down fa-5x triangle\"></div></div>"));
+        $(".galaryBackground").append($.parseHTML("<div class=\"galaryBut galaryNext\"><div class=\"fa fa-caret-down fa-5x triangle\"></div></div>"));
+      }
+      $(".galaryBackground").css("background-color", backColor);
+      console.log($(".galaryBackground").css("background-color"));
+      imgArray.each(function () {
         var self = this;
-
+        $(this).attr("data-index", i);
+        allImg.push({
+          index: i++,
+          src: $(self).attr("src")
+        });
         /**
         * add items to the DOM
         */
-        return $(this).on('click', function () {
+        return $(self).on('click', function () {
           var bigImg = $('.galaryBackground').css("display", "block");
           $(".bigImg img").attr("src", this.src);
-
-          if (defaulSettings.button == false) {
-              $(".galaryBut").css("display", "none");
-          }
           /**
           * close galary
           */
@@ -49,39 +67,49 @@
           /**
           * button next img and prev img
           */
-          if (defaulSettings.button == true) {
-            $('.galaryBut').on('click', function(){
-              console.log("fuck");
-              var button = $(this).attr('class').split(' '),
-              now = $(".bigImg img").attr("src"),
-              next,
-              prev;
-              for(var i = 0; i < allImg.length; i++){
-                if (i == allImg.length - 1) {
-                  next = allImg[0].src;
-                  prev = allImg[i - 1].src;
-                  break;
-                }
-                if (allImg[i].src == now && i == 0) {
-                  prev = allImg[allImg.length - 1].src;
-                  next = allImg[i + 1].src;
-                  break;
-                }
-                if (allImg[i].src == now) {
-                  next = allImg[i + 1].src;
-                  prev = allImg[i - 1].src;
-                  break;
-                }
-              }
-              if (button[1] == "galaryNext") {
-                $(".bigImg img").attr("src", next);
-                console.log(next);
-              }
-              else{
-                $(".bigImg img").attr("src", prev);
+          function nextPrev(where, auto){
+            if (auto == false) {
+              clearInterval(autoplay);
+            }
+            var nowSrc = $(".bigImg img").attr("src").split('/'),
+            nowSrc = nowSrc[nowSrc.length - 2] + "/" + nowSrc[nowSrc.length - 1];
+            $(allImg).each(function(){
+              if (this.src == nowSrc) {
+                nowIndex = this.index;
               }
             });
+            next = allImg[nowIndex + 1],
+            prev = allImg[nowIndex - 1];
+            if(where == "next") {
+              if(nowIndex == allImg.length - 1) {
+                next = allImg[0]
+              }
+              $(".bigImg img").attr("src", next.src);
+            }
+            else{
+              if(nowIndex == 0) {
+                prev = allImg[allImg.length - 1];
+              }
+              $(".bigImg img").attr("src", prev.src);
+            }
           }
+          if (defaulSettings.autoplay.bool == true) {
+            var autoplay = setInterval(function(){
+              nextPrev("next", true);
+            }, defaulSettings.autoplay.interval);
+          }
+          $('.galaryBut').on('click', function(){
+            var button = $(this).attr('class').split(' ')[1];
+            if (button == "galaryNext") {
+              nextPrev("next", false);
+            }
+            else{
+              nextPrev("prev", false);
+            }
+          });
+          $('.bigImg').on('click', function(){
+            nextPrev("next", false);
+          });
         });
       });
     };
