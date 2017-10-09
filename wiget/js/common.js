6d88b120e8fd013b7c0d89b838fbb691e9c8a3f9
Kirigam <hrytsiv.a.s@gmail.com>
2017-10-09 17:20:22 +0300

    Refact smAcc widget

--- wiget/js/common.js
+++ wiget/js/common.js
@@ -1,10 +1,12 @@
 $(document).ready( function () {
-    $('.accordion div').hide().prev().click(function () {
-        $('.accordion div').not(this).slideUp();
-        $(this).next().not(':visible').slideDown();
-    });
+    $('.accordion li').children("h3").smAcc({
+        onCl: function () {
+            $('.accordion div').hide();
+            $('.accordion div').not(this).slideUp();
+            $(this).next().not(':visible').slideDown();
+        }
+        });
     $('.fa-times').click(function () {
         $(this).parents('li').remove();
     });
-    $(accordion)
 });
--- wiget/js/smAccardion.jQuery.js
+++ wiget/js/smAccardion.jQuery.js
@@ -1,20 +1,24 @@
 (function ($) {
     $.widget('ui.smAcc', {
         options: {
-            onClickBefore: null,
-            onClickAfter: null
+            onCl: null
         },
 
         _create: function() {
             var element = this.element;
+            this.setGeneralClass();
             this._on(element, {
                 click: "onClick"
             });
         },
 
         onClick: function () {
-            this._trigger( "onClickBefore" );
-            this._trigger( "onClickAfter" );
+            this._trigger( "onCl" );
         },
+
+        setGeneralClass: function () {
+            var element = this.element.next();
+            element.addClass("notActive");
+        }
     });
 }(jQuery));
