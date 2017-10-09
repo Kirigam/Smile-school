a2fc3c0e6b2235386c6ca962a1d23ec8ab9c0a95
Andrew.Dol <adolsnskiy@gmail.com>
2017-10-09 19:11:22 +0300

    test

--- task_4/lib/js/select.milka/test.jQuery.js
+++ task_4/lib/js/select.milka/test.jQuery.js
@@ -1,54 +1,21 @@
 (function($) {
    $.widget( "ui.test", {
-
-      options: {
-         elementStyle:{
-            // "font": "Roboto",
-            // "color": "blue"
-         },
-
-         onClickBefore: null,
-         onClickAfter: null,
-         random: null
-      },
-
       _create: function() {
          var element = this.element;
          this.setGeneralClass();
-         this.levelAccordion();
          this._on(element.find(".title")[0], {
             click: "onClick",
          });
-         this._refresh();
-      },
-
-      _setOption: function(key, value) {         
-         if ($.inArray(key, Object.keys(this.options.elementStyle)) != -1) {
-            if (value == "random") {
-               value = this.random();
-            }
-            this.options.elementStyle[key] = value;
-            this._refresh();
-            return;
-         }
-         this._super(key, value);
-         this._refresh();
       },
       onClick: function () {
-         this._trigger( "onClickBefore" );
-         this.option({'color': 'black'});
-         this._trigger( "onClickAfter" );
          this.initAccordion();
-      },
-      _refresh: function () {
-         this.element.css(this.options.elementStyle);
       },
       initAccordion: function(){
          var self = this,
          opened = $('.open'); 
          if (opened.length > 0) {
             opened.each(function(){
-               if (self.element.attr("data-level") <= $(this).attr("data-level")) {
+               if (self.element.data("level") <= $(this).data("level")) {
                   self.accordion($(this));
                }
             });
@@ -84,13 +51,13 @@
       setGeneralClass: function () {
          var element = this.element,
          i = 1,
-         level = parseInt(element.attr("data-level"));
+         level = parseInt(element.data("level"));
          if (level > 0) {
             i = level;
          }
          element.addClass("my-widget-test");
-         element.attr("data-level", i);
-         element.find(".testWidget").attr("data-level", ++i);
+         element.data("level", i);
+         element.find(".testWidget").data("level", ++i);
       },
    });
 }(jQuery));
